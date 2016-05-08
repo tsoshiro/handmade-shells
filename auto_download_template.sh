db487552d6812895f40ad7f6b9b1d626007502dc
toshiro <tsubasa.oshiro@gmail.com>
2016-05-08 14:46:33 +0900

    まとめてダウンロードのシェルスクリプト

---
+++ auto_download_template.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#ファイルの命名規則が分かっている複数のファイルをまとめてダウンロードする
+#何番目まで取得するか指定	
+NUM_SERIAL=10
+#ファイル元のURL
+BASE_URL="http://xxx.yyy.zzz"
+#ファイルの拡張子
+BASE_EXT=".mp3"
+#ローカルのダウンロードリンク	
+DL_DIR="your/local/directory"
+for i in `seq -f "%04g" 1 ${NUM_SERIAL}`
+do
+wget -P $DL_DIR -nc $BASE_URL$i$BASE_EXT
+done
